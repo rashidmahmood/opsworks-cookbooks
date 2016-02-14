b5bc6ff031cc4439a5eab9aa89092b3940b3a56a
rashidmahmood <xs2rashid@live.com>
2016-02-14 21:17:24 +0500

    update 2

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -9,3 +9,16 @@
 execute "yum update" do
   command "yum update"
 end
+
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/usr/share/nginx/www/index.html" do
+  source "index.html"
+  mode "0644"
+end
