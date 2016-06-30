4d3a7af1cade8b876d0eb7e4221eb15e23458d32
rashidmahmood <xs2rashid@live.com>
2016-06-30 20:36:16 +0500

    Iptables rules changed

--- op-iptables/files/default/iptables.sh
+++ op-iptables/files/default/iptables.sh
@@ -1,13 +1,11 @@
 iptables -t filter -F
 iptables -t nat -F
 iptables -t mangle -F
-iptables -A INPUT -s 172.23.0.0/16 -j ACCEPT
 iptables -A INPUT -s 172.22.0.0/16 -j ACCEPT
 iptables -A INPUT -s 10.0.0.0/16 -j ACCEPT
 #Enable ICMP
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
-
 #Accept loopback addresses
 iptables -A INPUT -i lo -j ACCEPT
 # Accept packets belonging to established and related connections
